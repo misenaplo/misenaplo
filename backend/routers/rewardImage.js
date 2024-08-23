d96f48180b133e520fce0303592560bbb17897f9
Vörös László <vorosl@inf.u-szeged.hu>
2024-08-24 00:21:08 +0200

    header

--- backend/routers/rewardImage.js
+++ backend/routers/rewardImage.js
@@ -34,8 +34,8 @@ module.exports = function (passport, sequelize, mailer, middlewares, roles, code
         if(reward===null) {
             return res.status(404).json(errorGenerator.INSTANCE_NOT_FOUND("Nem található jutalomkép"));
         }
-        res.setHeaders("Content-Type", reward.Medium.metadata.mimeType)
-        res.setHeaders("Content-Disposition", contentDisposition(reward.Medium.metadata.originalName))
+        res.setHeader("Content-Type", reward.Medium.metadata.mimeType)
+        res.setHeader("Content-Disposition", contentDisposition(reward.Medium.metadata.originalName))
         res.send(Buffer.from(reward.Medium.buffer, 'base64'))
     })
 
